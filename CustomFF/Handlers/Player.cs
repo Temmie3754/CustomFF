c8407fa7db7a074ecc100b580088a4023d6152c6
TemmieGamerGuy <smith.nathaniel@gmail.com>
2021-09-04 16:43:28 +0100

    fixed possible tesla bug

--- CustomFF/Handlers/Player.cs
+++ CustomFF/Handlers/Player.cs
@@ -7,7 +7,7 @@ namespace CustomFF.Handlers
         public void OnHurting(HurtingEventArgs ev)
         {
             // Checks if the attacker is allowed to hurt the target or if the target is cuffed and server disables harming cuffed enemies
-            if (CustomFF.Instance.Config.DisabledDamage[ev.Attacker.Team].Contains(ev.Target.Team) || (ev.Target.IsCuffed && CustomFF.Instance.Config.DisableCuffedDamage))
+            if (ev.HitInformation.IsPlayer && (CustomFF.Instance.Config.DisabledDamage[ev.Attacker.Team].Contains(ev.Target.Team) || (ev.Target.IsCuffed && CustomFF.Instance.Config.DisableCuffedDamage)))
             {
                 ev.IsAllowed = false;
             }
